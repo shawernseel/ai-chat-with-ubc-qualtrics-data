3b229902870fb4f7882696d71a8d782ddc507bbb
shawernseel <shawern101@gmail.com>
2024-08-10 14:31:22 -0700

    streamlit innit

--- src/app.py
+++ src/app.py
@@ -2,3 +2,7 @@ from dotenv import load_dotenv
 import streamlit as st
 
 load_dotenv()
+
+st.set_page_config(page_title="Chat with MySQL", page_icon=":speech_balloon:") #streamlit webapp setup
+
+st.title("Chat with MySQL")
