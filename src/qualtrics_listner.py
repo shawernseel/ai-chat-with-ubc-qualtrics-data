79afaa544595680b1f260da1d7be47a243f953aa
shawernseel <shawern101@gmail.com>
2024-08-12 23:17:01 -0700

    set up qualtrics keys

--- src/qualtrics_listner.py
+++ src/qualtrics_listner.py
@@ -5,3 +5,11 @@ import sys
 import requests
 import os
 import simplejson as json
+from dotenv import load_dotenv
+load_dotenv()
+
+survey_id = os.environ['SURVEY_ID']
+api_key = os.environ['API_KEY']
+data_center = os.environ['DATA_CENTER']
+
+print(survey_id, api_key, data_center)
